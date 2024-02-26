fc74d1864ddc3284d8da20f25bda6bf5c4fb7ac8
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2024-02-26 19:52:28 +0530

    fetch done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  const fetchProducts = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const json = await res.json();
+    setProducts(json?.products);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.map((product) => {
+            return (
+              <span key={product.id} className="App">
+                <img src={product.thumbnail} alt={product.title} />
+                <span>{product.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
