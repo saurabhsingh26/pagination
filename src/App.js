18e859ece8fc1b25910e44e1db6cf70464f5633f
thesaurabhsingh <80765330+thesaurabhsingh@users.noreply.github.com>
2024-02-26 20:47:44 +0530

    pagination done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
-  console.log(products);
+  const [page, setPage] = useState(1);
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products?limit=100");
     const json = await res.json();
@@ -12,11 +12,14 @@ function App() {
     fetchProducts();
   }, []);
 
+  const selectPageHandler = (selectedPage) => {
+    setPage(selectedPage);
+  };
   return (
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((product) => {
+          {products.slice(page * 10 - 10, page * 10).map((product) => {
             return (
               <span key={product.id} className="App">
                 <img src={product.thumbnail} alt={product.title} />
@@ -24,6 +27,26 @@ function App() {
               </span>
             );
           })}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="pagination">
+          {page > 1 && (
+            <span onClick={() => selectPageHandler(page - 1)}>◀️</span>
+          )}
+
+          {[...Array(products.length / 10)].map((_, i) => (
+            <span
+              className={page === i + 1 ? "pagination__selected" : ""}
+              onClick={() => selectPageHandler(i + 1)}
+              key={i}
+            >
+              {i + 1}
+            </span>
+          ))}
+          {page < products.length / 10 && (
+            <span onClick={() => selectPageHandler(page + 1)}>▶️</span>
+          )}
         </div>
       )}
     </div>
